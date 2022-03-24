ac878756ae52fc63e2268de3b0577fc284a669b3
yongwoo-jeong <jywoo9@gmail.com>
2022-03-24 11:14:30 +0900

    build: Init NextJS with react18 RC, tailwindCSS set up

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-black">It works</h1>
+    </div>
+  );
+};
+
+export default Home;
