ac0787f0e1194c70eaf43fd1ec9e2f1c8b94682d
yongwoo-jeong <jywoo9@gmail.com>
2022-03-26 20:03:49 +0900

    feat: Add transitions to 3 button  //  NextJS version change

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,9 +17,12 @@ const Home: NextPage = () => {
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+        <button
+          className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto block
+         hover:bg-teal-500 hover:text-black active:bg-yellow-500"
+        >
           Check Out
-        </div>
+        </button>
       </div>
       <div className="bg-white overflow-hidden rounded-2xl shadow-2xl">
         <div className="bg-blue-500 p-6 pb-14">
@@ -56,10 +59,10 @@ const Home: NextPage = () => {
           <span className="font-medium mb-1.5 text-xl ">Swoon Lounge</span>
           <span className="text-xs text-gray-500">Chair</span>
           <div className="mt-3 mb-5 flex justify-between items-center">
-            <div>
-              <input type="radio" />
-              <input type="radio" />
-              <input type="radio" />
+            <div className="space-x-2">
+              <button className="w-5 h-5 rounded-full bg-yellow-500 focus:ring-2 ring-yellow-500 ring-offset-2 transition" />
+              <button className="w-5 h-5 rounded-full bg-indigo-500 focus:ring-2 ring-indigo-500 ring-offset-2 transition" />
+              <button className="w-5 h-5 rounded-full bg-teal-500 focus:ring-2 ring-teal-500 ring-offset-2 transition" />
             </div>
             <div className="flex items-center space-x-5">
               <button className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 text-xl text-gray-500 ">
