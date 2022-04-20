325fec17d66b36ae70f3bd7a1d6fdee91e9fe4d0
yongwoo-jeong <jywoo9@gmail.com>
2022-04-20 15:36:38 +0900

    feat: Chat detail styling

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -4,7 +4,10 @@ const Chats: NextPage = () => {
   return (
     <div className="divide-y-[1px] py-10">
       {[...Array(10)].map((_, i) => (
-        <div className="flex cursor-pointer items-center space-x-3 px-4 py-3">
+        <div
+          key={i}
+          className="flex cursor-pointer items-center space-x-3 px-4 py-3"
+        >
           <div className="h-12 w-12 rounded-full bg-slate-300" />
           <div>
             <p className="text-gray-700">Steve Jebs</p>
