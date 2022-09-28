9ce23b51df51d9c85acf4d2197b6adceaf064972
yongwoojeong <jywoo9@gmail.com>
2022-09-28 11:18:51 +0900

    Enter page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,88 +1,114 @@
-import type { NextPage } from "next";
+import { useState } from "react";
 
-const Home: NextPage = () => {
+function cls(...classnames: string[]) {
+  return classnames.join(" ");
+}
+
+export default function Enter() {
+  const [method, setMethod] = useState<"email" | "phone">("email");
+  const onEmailClick = () => setMethod("email");
+  const onPhoneClick = () => setMethod("phone");
   return (
-    <div className="grid min-h-screen gap-10 space-y-5 bg-slate-400 px-10 py-10 lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center">
-      <div className="flex flex-col justify-between rounded-3xl bg-white p-6 shadow-xl ">
-        <span className="text-3xl font-semibold">Select Item</span>
-        <ul>
-          {[1, 2, 3, 4, 5].map((i) => (
-            <div className="first my-2 flex justify-between" key={i}>
-              <span className="text-gray-500">Grey Chair</span>
-              <span className="font-semibold">$19</span>
-            </div>
-          ))}
-        </ul>
-        <div className="flex flex-col">
-          <div className="mt-2 flex justify-between border-t-2 border-dashed pt-2">
-            <span>Total</span>
-            <span className="font-semibold">$91</span>
+    <div className="mt-16  px-4">
+      <h3 className="text-center text-3xl font-bold">Enter to Carrot</h3>
+      <div className="mt-8">
+        <div className="flex flex-col items-center">
+          <h5 className="text-sm font-medium text-gray-500">Enter using:</h5>
+          <div className="mt-8 grid w-full grid-cols-2 gap-16 border-b  ">
+            <button
+              className={cls(
+                "border-b-2 pb-4 font-medium",
+                method === "email"
+                  ? " border-orange-500 text-orange-400"
+                  : "border-transparent text-gray-500"
+              )}
+              onClick={onEmailClick}
+            >
+              Email
+            </button>
+            <button
+              className={cls(
+                "border-b-2 pb-4 font-medium",
+                method === "phone"
+                  ? " border-orange-500 text-orange-400"
+                  : "border-transparent text-gray-500"
+              )}
+              onClick={onPhoneClick}
+            >
+              Phone
+            </button>
           </div>
-          <button className="m-auto mx-auto w-3/5 rounded-xl bg-blue-500 p-2 text-center text-white hover:bg-teal-500 hover:text-black ">
-            Check out
+        </div>
+        <form className="mt-8 flex flex-col">
+          <label className="text-sm font-medium text-gray-700">
+            {method === "email" ? "Email address" : null}
+            {method === "phone" ? "Phone number" : null}
+          </label>
+          <div className="mt-2">
+            {method === "email" ? (
+              <input
+                type="email"
+                className="py- focus: w-full 
+                appearance-none rounded-md border-gray-300 py-3 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+                required
+              />
+            ) : null}
+            {method === "phone" ? (
+              <div className="flex rounded-md shadow-sm">
+                <span className="flex select-none items-center justify-center rounded-l-md border border-r-0  border-gray-300 bg-gray-50 px-4 text-sm text-gray-500">
+                  +82
+                </span>
+                <input
+                  type="number"
+                  className="py- focus: w-full 
+                appearance-none rounded-md rounded-l-none border-gray-300 py-3 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
+                  required
+                />
+              </div>
+            ) : null}
+          </div>
+          <button className="mt-5 rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
+            {method === "email" ? "Get login link" : null}
+            {method === "phone" ? "Get one-time password" : null}
           </button>
-        </div>
-      </div>
-      <div className="group rounded-2xl  bg-white shadow-xl  ">
-        <div className="overflow-hidden rounded-t-2xl bg-blue-500 p-6 pb-14 lg:pb-28 xl:pb-52">
-          <span className="text-2xl text-white">Profile</span>
-        </div>
-        <div className="relative  -top-5 rounded-3xl bg-white p-6">
-          <div className="itend relative -top-16 flex items-end justify-between">
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Order</span>
-              <span className="font-medium">340</span>
-            </div>
-            <div className="h-24 w-24  rounded-full bg-zinc-400 transition-colors group-hover:bg-red-400" />
-            <div className="flex flex-col items-center">
-              <span className="text-sm text-gray-500">Spent</span>
-              <span className="font-medium">$340</span>
+        </form>
+        <div className="mt-8">
+          <div className="relative">
+            <div className="absoulte w-full border-t border-gray-300" />
+            <div className="relative -top-4 text-center">
+              <span className=" bg-white px-2 text-sm text-gray-500 ">
+                Or enter with
+              </span>
             </div>
           </div>
-          <div className="relative -mt-10 -mb-5 flex flex-col items-center">
-            <span className="text-lg font-medium">Tony Molloy</span>
-            <span className="text-sm text-gray-400">뉴욕, 미국</span>
-          </div>
-        </div>
-      </div>
-      <div className="rounded-2xl bg-white p-10 shadow-xl lg:col-span-2  xl:col-span-1  ">
-        <div className="mb-5 flex items-center justify-between">
-          <span>⬅</span>
-          <div className="space-x-3">
-            <span>⭐️ 4.9</span>
-            <span className="rounded-md p-2 text-red-500 shadow-xl">♥</span>
-          </div>
-        </div>
-        <div className="mb-5 h-72 bg-zinc-400" />
-        <div className="flex flex-col">
-          <span className="mb-1.5 text-xl font-medium">Swoon Lounge</span>
-          <span className="to-gray-500 text-xs">Chair</span>
-          <div className="mt-3 mb-5 flex items-center justify-between">
-            <div className="space-x-2">
-              <button className="h-5 w-5 rounded-full bg-yellow-500 bg-opacity-50 ring-yellow-500 ring-offset-2 transition-all focus:ring-2" />
-              <button className="h-5 w-5 rounded-full bg-indigo-500 bg-opacity-50 ring-indigo-500 ring-offset-2 transition-all focus:ring-2" />
-              <button className="h-5 w-5 rounded-full bg-teal-500 bg-opacity-50 ring-teal-500 ring-offset-2 transition-all focus:ring-2" />
-            </div>
-            <div className="flex items-center space-x-4">
-              <button className="aspect-square w-8 items-center justify-center rounded-lg bg-blue-100 p-1.5 text-xl font-medium text-gray-500">
-                -
-              </button>
-              <span>1</span>
-              <button className="aspect-square w-8 items-center justify-center rounded-lg bg-blue-100 p-1.5 text-xl font-medium text-gray-500">
-                +
-              </button>
-            </div>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="text-2xl font-medium">$450</span>
-            <button className="rounded-lg bg-blue-500 py-2 px-8 text-center text-sm text-white">
-              Add to cart
+          <div className=" mt-2 grid grid-cols-2 gap-3">
+            <button className="flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path d="M6.29 18.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0020 3.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.073 4.073 0 01.8 7.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 010 16.407a11.616 11.616 0 006.29 1.84" />
+              </svg>
+            </button>
+            <button className="flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-500 shadow-sm hover:bg-gray-50">
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
             </button>
           </div>
         </div>
       </div>
     </div>
   );
-};
-
-export default Home;
+}
