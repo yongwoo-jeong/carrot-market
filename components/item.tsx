686b59ffce4efaf9989d59d42e2fcb2b1e013def
yongwoojeong <jywoo9@gmail.com>
2022-10-06 00:20:24 +0900

    Replace to item component

--- components/item.tsx
+++ components/item.tsx
@@ -22,7 +22,7 @@ export default function Item({
           <div className="h-20 w-20 rounded-md bg-gray-400" />
           <div className="flex flex-col pt-2">
             <h3 className="text-sm font-medium text-gray-900">{title}</h3>
-            <span className="mt-1 font-medium text-gray-900">${price}</span>
+            <span className="gray-900 mt-2 text-sm font-medium">$ {price}</span>
           </div>
         </div>
         <div className="flex items-end justify-end space-x-2">
