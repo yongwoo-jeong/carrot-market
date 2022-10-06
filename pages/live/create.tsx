4be06da72909a26e59ec4bc5e42e62d147ee6c42
yongwoojeong <jywoo9@gmail.com>
2022-10-06 10:11:39 +0900

    Done Live page into Comp

--- pages/live/create.tsx
+++ pages/live/create.tsx
@@ -1,60 +1,26 @@
 import { NextPage } from "next";
+import Button from "../../components/button";
+import Input from "../../components/input";
+import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const StreamCreate: NextPage = () => {
   return (
-    <div className="space-y-5 px-4 py-10">
-      <div>
-        <label
-          className="mb-1 block text-sm font-medium text-gray-700"
-          htmlFor="name"
-        >
-          Name
-        </label>
-        <div className="relative flex items-center  rounded-md shadow-sm">
-          <input
-            id="name"
-            type="email"
-            className="w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
-            required
-          />
-        </div>
-      </div>
-      <div className="my-5">
-        <label
-          htmlFor="price"
-          className="mb-1 block text-sm font-medium text-gray-700"
-        >
-          Price
-        </label>
-        <div className="shadow-s relative flex items-center rounded-md">
-          <div className="pointer-events-none absolute left-0 flex items-center justify-center pl-3">
-            <span className="text-sm text-gray-500">$</span>
-          </div>
-          <input
-            className="py- focus: w-full 
-                appearance-none rounded-md border-gray-300 py-3 pl-7 placeholder-gray-400 shadow-sm focus:border-orange-500 focus:outline-none focus:ring-orange-500"
-            id="price"
-            type="text"
-            placeholder="0.00"
-          />
-          <div className="pointer-events-none absolute right-0 flex items-center pr-3">
-            <span className="text-gray-500">USD</span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <label className="mb-1 block text-sm font-medium text-gray-700">
-          Description
-        </label>
-        <textarea
-          className="mt-1 w-full rounded-md border-gray-300 shadow-sm focus:border-orange-500 focus:ring-2 focus:ring-orange-500"
-          rows={4}
+    <Layout canGoBack title="라이브 시작">
+      <form className="space-y-5 px-4 py-10">
+        <Input required label="Name" name="name" type="text" />
+        <Input
+          required
+          label="Price"
+          placeholder="0.00"
+          name="price"
+          type="text"
+          kind="price"
         />
-      </div>
-      <button className="mt-3 w-full rounded-md border border-transparent bg-orange-500 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2">
-        Upload product
-      </button>
-    </div>
+        <TextArea name="description" label="Description" />
+        <Button text="시작" />
+      </form>
+    </Layout>
   );
 };
 
